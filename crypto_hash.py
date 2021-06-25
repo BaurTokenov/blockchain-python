60064ab47b7518bd7fccc378ef99a766dc1882d1
BaurTokenov <baur.tokenov@kaist.ac.kr>
2021-06-25 22:19:08 +0600

    SHA-256 hashing

---
+++ crypto_hash.py
@@ -0,0 +1,24 @@
+import hashlib
+import json
+
+
+def crypto_hash(*args):
+    """
+    Return a sha-256 hash of the given arguments.
+    """
+
+    str_args = sorted([json.dumps(arg) for arg in args])
+
+    joined_data = "".join(str_args)
+
+    print(f"joined_data: {joined_data}")
+    return hashlib.sha256(joined_data.encode("utf-8")).hexdigest()
+
+
+def main():
+    print(f"crypto_hash(1): {crypto_hash('foo', [12, 13], 'sobaka', 15)}")
+    print(f"crypto_hash(2): {crypto_hash('foo',  'sobaka', [12, 13], 15)}")
+
+
+if __name__ == "__main__":
+    main()
